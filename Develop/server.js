3abe7b9df756127b262595f0f315a22c6a681b35
unknown <jason.rettinger@gmail.com>
2022-09-15 09:42:26 -0400

    added more code to server.js

--- Develop/server.js
+++ Develop/server.js
@@ -6,3 +6,20 @@ const fs = require("fs");
 const shortid = require("shortid");
 
 const app = express();
+
+// middlewares
+app.use(express.static(path.join(__dirname, "/public")));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// routes
+/*  
+    route - /notes
+    method - GET
+    access public
+*/
+
+app.get("/notes", (req, res) => {
+    res.sendFile(path.join(__dirname, "/public/notes.html"));
+  });
+  
